5fa3d677b2ca5570d64d612f550dc71b8523f7b5
Alicja Choda <s34446@pjwstk.edu.pl>
2026-03-18 22:17:33 +0100

    Dodanie operatora ternarnego do CalculateAverage

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -1,2 +1,1 @@
-public class StatisticsHelper { public int GetMin(int[] tab1) =; } 
-public double CalculateAverage(int[] values) =
+public class StatisticsHelper { public int GetMin(int[] t) =; public double CalculateAverage(int[] v) { return v.Length == 0 ? 0 : 0.0; } } 
